944eaf0ca3104d6e90a10e2cc832badf6a0e27c4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-15 20:46:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,14 +26,24 @@ function App() {
       <h2 className="text-3xl font-semibold text-center mt-2 mb-4">
         Good Morning, Gerges
       </h2>
-      {articles.map((article, idx) => (
-        <Feed
-          title={article.item.title}
-          link={article.item.link}
-          date={article.item.pubDate}
-          key={idx}
-        />
-      ))}
+
+      <div className="w-3/4 max-w-lg border mx-auto p-5 rounded-xl ">
+        <div className="bg-black flex flex-col items-center border rounded-lg p-2 mb-5">
+          <img
+            className=""
+            src="https://cdn-images-1.medium.com/max/273/1*rOPLUJ3W6FUA3rO1U1IeuA@2x.png"
+            alt="netflix_TechBlog"
+          />
+        </div>
+        {articles.map((article, idx) => (
+          <Feed
+            title={article.title}
+            link={article.link}
+            date={article.pubDate}
+            key={idx}
+          />
+        ))}
+      </div>
     </>
   );
 }
