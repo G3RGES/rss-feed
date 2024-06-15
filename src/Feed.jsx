2b8e2cf31b0842fa307afadbf30bc7f71d9f71f6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-15 20:46:50 +0300

    Update Feed.jsx

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Feed = ({ title, link, date }) => {
-  return <>Feed</>;
+  let formattedDate = { day: "numeric", month: "long", year: "numeric" };
+
+  let articleDate = new Date(date).toLocaleDateString("en-EG", formattedDate);
+
+  return (
+    <>
+      <a
+        className="hover:text-blue-700"
+        target="_blank"
+        href={link}
+        rel="noreferrer"
+      >
+        <h3 className="text-xl mb-1 font-semibold">{title}</h3>
+      </a>
+      <p className="">{articleDate}</p>
+    </>
+  );
 };
 
 export default Feed;
