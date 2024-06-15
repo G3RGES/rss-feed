bded31593b6f4722a089243e6d992a63fb51191f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-15 13:56:03 +0300

    Update index.js

--- rss-server/index.js
+++ rss-server/index.js
@@ -7,13 +7,16 @@ const feedURL = "https://netflixtechblog.com/feed";
 
 const parser = new RSSParser();
 
+let articles = [];
+
 const parse = async (url) => {
   const feed = await parser.parseURL(url);
 
-  console.log(feed.title);
+  //   console.log(feed.title);//*TESTING
 
   feed.items.forEach((item) => {
-    console.log(`${item.title}\n ${item.link}\n\n `);
+    //   console.log(`${item.title}\n ${item.link}\n\n `)//*TESTING
+    articles.push(item);
   });
 };
 
@@ -21,6 +24,10 @@ parse(feedURL);
 
 let app = express();
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(articles);
+});
 
 const server = app.listen("4000", () => {
   console.log("App is Listening");
