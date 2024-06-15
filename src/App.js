1bb66d833d1b515bec74b6eaad86fc5ffd979107
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-15 20:24:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Feed from "./Feed";
+
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  console.log(articles); //*TESTING
+
   return (
     <>
-      <h1 className="">RSS-FEED</h1>
+      <h1 className="text-center text-xl font-semibold mt-5">RSS-FEED</h1>
+      <h2 className="text-3xl font-semibold text-center mt-2 mb-4">
+        Good Morning, Gerges
+      </h2>
+      {articles.map((article, idx) => (
+        <Feed
+          title={article.item.title}
+          link={article.item.link}
+          date={article.item.pubDate}
+          key={idx}
+        />
+      ))}
     </>
   );
 }
